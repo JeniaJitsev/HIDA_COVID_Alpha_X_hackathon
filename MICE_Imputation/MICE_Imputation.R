d05d3496ebd918677f4f5aae9a7ac9b08dbfef97
Marco Stock <stk.mrc@gmail.com>
2021-04-29 14:01:54 +0200

    Added MICE imputation files for metric benchmarking

--- MICE_Imputation/MICE_Imputation.R
+++ MICE_Imputation/MICE_Imputation.R
@@ -1,6 +1,11 @@
 trainSet <- read.table("./trainSet/trainSet.txt", header=T, sep=",", na.strings=c("NaN","<undefined>"))
 testSet <- read.table("./testSet/testSet.txt", header=T, sep=",", na.strings=c("NaN","<undefined>"))
 totalSet <- rbind(trainSet, testSet)
+totalSet$Cough <- as.factor(totalSet$Cough)
+totalSet$DifficultyInBreathing <- as.factor(totalSet$DifficultyInBreathing)
+totalSet$CardiovascularDisease <- as.factor(totalSet$CardiovascularDisease)
+totalSet$RespiratoryFailure <- as.factor(totalSet$RespiratoryFailure)
+totalSet$Prognosis <- as.factor(totalSet$Prognosis)
 
 library(finalfit)
 library(dplyr)
@@ -86,3 +91,87 @@ write.table(completedTestSet, file="./testSet/testSet_Imputed.txt", sep=",", quo
 
 sum(is.na(completedTrainSet))
 sum(is.na(completedTestSet))
+
+imputeMICE <- function(dataset, methods) {
+  tempDataframe <- mice(dataset, predictorMatrix=predM, m=1, maxit=10, method=methods)
+  return(completedDataframe <- complete(tempDataframe, 1))
+}
+
+#######Evaluation#########
+
+sigmas <- c(
+  #"Age": 
+  15.05,
+  #"Temp_C"
+  0.97,
+  #"WBC"
+  3.53,
+  #"CRP"
+  66.93,
+  #"Fibrinogen"
+  158.36,
+  #"LDH"
+  235.22,
+  #"Ddimer"
+  6743.13,
+  #"Ox_percentage"
+  7,
+  #"PaO2"
+  26.11,
+  #"SaO2"
+  8.24,
+  #"pH"
+  0.06,
+  #"Sex"
+  1,
+  #"Cough"
+  1,
+  #"DifficultyInBreathing"
+  1,
+  #"CardiovascularDisease"
+  1,
+  #"RespiratoryFailure"
+  1
+)
+type <- c("str", "str", "str", "num", "str", "num", "str", "str", rep("num", 9), "str", "str", "str")
+
+filenames <- list.files("/Users/marco.stock/Documents/COVID/HIDA_COVID_Alpha_X_hackathon/data/train_set_with_missing_vals", pattern="*.txt", full.names=TRUE)
+ldf <- lapply(filenames, read.table, header=T, sep=",", na.strings=c("NaN","<undefined>"))
+
+evaluate <- function(groundTruthc, imputedc, missingc, sigma, typec) {
+  m1<- rep(2, length(groundTruthc))
+  if(typec=="str") {
+    m1 <- sapply((((as.integer(as.logical(groundTruthc == imputedc))) / sigma)^2 ), min, 1)
+  }
+  else if(typec=="num"){
+    m1 <- sapply((((groundTruthc - imputedc) / sigma)^2 ), min, 1)
+  }
+  #print(m1)
+  #print(missingc)
+  #print(m1[missingc])
+  print(mean((m1[missingc])))
+  return(mean(m1[missingc]))
+}
+
+ImputeandEval <- function(methods) {
+  mean <- rep(2, length(ldf))
+  for(i in 1:length(ldf)) {
+    toImpute <- ldf[[i]]
+    #print(filenames[[i]])
+    imputed <- imputeMICE(toImpute, methods)
+    write.table(imputed, file=paste(filenames[[i]],"_MICE_RF.txt"), sep=",", quote = FALSE, row.names=F)
+    metrics <- rep(2, length(sigmas))
+    for(c in 1:length(sigmas)) {
+      #print(paste("Spalte", colnames(groundTruth[c])))
+      metrics[c] <- evaluate(groundTruth[,(c+3)], imputed[,(c+3)], (is.na(toImpute[,(c+3)])&!is.na(groundTruth[,(c+3)])), sigmas[c], type[(c+3)])
+    }
+    mean[i] <- mean(metrics)
+  }
+  return(mean)
+}
+
+groundTruth <- totalSet[(1:nrow(trainSet)),]
+methods <- list(rep("rf", ncol(groundTruth)))
+sapply(methods, ImputeandEval)
+
+
